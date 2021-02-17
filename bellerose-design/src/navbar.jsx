023ee2bd8ced2e5b1d30d5157a78c95b15f8dfa6
Kim Duclos <kimduclos@gmail.com>
2021-02-17 15:02:49 -0800

    non-working transition

--- bellerose-design/src/navbar.jsx
+++ bellerose-design/src/navbar.jsx
@@ -8,7 +8,10 @@ import ServiceArea from "./service-area.jsx";
 import FeaturedProducts from "./featured-products.jsx";
 import About from "./about.jsx";
 import Contact from "./contact.jsx";
+// eslint-disable-next-line
 import ScrollToTop from "./scrolltotop.jsx";
+import "./packages/styles.css";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = React.useState(false);
@@ -30,55 +33,60 @@ const Navbar = () => {
   }
 
   return (
-    <Router>
-      <ScrollToTop />
-      <header className={navbarClasses.join(" ")}>
-        <nav className="navigation">
-          <ul>
-            <li>
-              <Link to="/">HOME</Link>
-            </li>
-            <li>
-              <Link to="/interior-design">INTERIOR DESIGN</Link>
-            </li>
-            <li>
-              <Link to="/service-area">SERVICE AREA</Link>
-            </li>
-            <li>
-              <Link to="/featured-products">FEATURED PRODUCTS</Link>
-            </li>
-            <li>
-              <Link to="/about">ABOUT</Link>
-            </li>
-            <li>
-              <Link smooth to="/contact">
-                CONTACT
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/interior-design">
-          <InteriorDesign />
-        </Route>
-        <Route path="/service-area">
-          <ServiceArea />
-        </Route>
-        <Route path="/featured-products">
-          <FeaturedProducts />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
-      </Switch>
-    </Router>
+    <TransitionGroup>
+      <CSSTransition key={window.location.key} className="fade-enter" timeout={300}>
+        <Router>
+          <ScrollToTop />
+          <header className={navbarClasses.join(" ")}>
+            <nav className="navigation">
+              <ul>
+                <li>
+                  <Link to="/">HOME</Link>
+                </li>
+                <li>
+                  <Link to="/interior-design">INTERIOR DESIGN</Link>
+                </li>
+                <li>
+                  <Link to="/service-area">SERVICE AREA</Link>
+                </li>
+                <li>
+                  <Link to="/featured-products">FEATURED PRODUCTS</Link>
+                </li>
+                <li>
+                  <Link to="/about">ABOUT</Link>
+                </li>
+                <li>
+                  <Link smooth to="/contact">
+                    CONTACT
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
+
+          <Switch location={window.location}>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/interior-design">
+              <InteriorDesign />
+            </Route>
+            <Route path="/service-area">
+              <ServiceArea />
+            </Route>
+            <Route path="/featured-products">
+              <FeaturedProducts />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
